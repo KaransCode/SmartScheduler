a2d5010b26f09b2df4d59eb08d26b9c1ffd2de52
Karan Singh <karan.ts265@gmail.com>
2026-04-17 10:45:37 +0530

    Initial commit: Completed SmartScheduler Pro Edition

---
+++ build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_SmartScheduler_FILE C:/College Projects/TT/SmartScheduler/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/SmartScheduler.exe)
+set(__QT_DEPLOY_TARGET_SmartScheduler_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_SmartScheduler_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
